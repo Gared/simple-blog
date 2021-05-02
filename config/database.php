53127e6bd53eba0d2ebc90c7bf83cf75ac471575
Stefan Müller <mu.stefan@googlemail.com>
2021-05-02 20:36:45 +0200

    Add example database config

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'mysql' => [
+        'dsn' => 'mysql:host=127.0.0.1;dbname=simple_blog;port=3306',
+        'password' => 'test',
+        'user' => 'test'
+    ]
+];
