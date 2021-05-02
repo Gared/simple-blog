1fb36dfa2878fa9af29b8a1a8d627a392d79942f
Stefan Müller <mu.stefan@googlemail.com>
2021-05-02 15:09:52 +0200

    Add PostOverviewController

---
+++ src/ExternalApi/Blog/Controller/PostOverviewController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StefanBlog\ExternalApi\Blog\Controller;
+
+use StefanBlog\Infrastructure\Controller\SimpleControllerInterface;
+
+class PostOverviewController implements SimpleControllerInterface
+{
+
+}
